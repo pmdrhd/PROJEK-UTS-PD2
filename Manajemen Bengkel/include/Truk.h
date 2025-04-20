6106b156bc19b5e424f3765d126eaa87a0d4e254
pmdrhd <rahadian1245@gmail.com>
2025-04-20 11:31:27 +0700

    Update Truk.h

--- Manajemen Bengkel/include/Truk.h
+++ Manajemen Bengkel/include/Truk.h
@@ -1,20 +1,14 @@
 #ifndef TRUK_H
 #define TRUK_H
 
-#include <iostream>
-#include <string>
+//#include <iostream>
+//#include <string>
 #include "Kendaraan.h"
 using namespace std;
 
 class Truk : public Kendaraan {
 public:
-
-	//Metode
-	void inputTipeTruk(void);
-
-	Truk() {
-		inputTipeTruk();
-	}
+	Truk();
 };
 
 #endif
